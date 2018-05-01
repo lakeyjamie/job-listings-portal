386e564297c2de25147ec18d601f2d670a71054f
Jamie Kent <jamie.kent510@gmail.com>
2018-05-01 13:44:57 -0300

    add links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
 	render() {
 		return (
 			<footer>
-				<p>This page is in beta.</p>
+				<p>Brought to you by <a href="http://womeninsportstech.org/" target="_blank" rel="noopener noreferrer">Women In Sports Tech</a> and <a href="https://www.sporttechie.com/" target="_blank" rel="noopener noreferrer">SportTechie</a>.</p>
 			</footer>
 		);
 	}
